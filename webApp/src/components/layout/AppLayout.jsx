e85d4bcef08b3b0a36e8070dc378ee4d8881ad21
Divyansh-Sinha <divyansh8252230@gmail.com>
2026-05-11 23:31:08 +0530

    feat: fixes

--- webApp/src/components/layout/AppLayout.jsx
+++ webApp/src/components/layout/AppLayout.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
 const AppLayout = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const location = useLocation();
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex min-h-screen">
       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      <div className="flex-1 flex flex-col overflow-y-auto h-screen">
+      <div ref={scrollRef} className="flex-1 flex flex-col overflow-y-auto h-screen">
         <Topbar />
         <div className="p-8 w-full max-w-[1200px] mx-auto">
           <Outlet />
